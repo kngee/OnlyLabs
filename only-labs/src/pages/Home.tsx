f188b8376df2736e9f62788c63aba1c97e8ae5a7
Bethlehem <bethlehemshimelis@gmail.com>
2026-04-24 20:50:03 +0200

    uod

--- only-labs/src/pages/Home.tsx
+++ only-labs/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { ChevronLeft, Settings, Code, RotateCcw, Pause, Play, Square } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
